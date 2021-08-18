d6668a6b4637065c79f3e7b9bef328de87ff2f52
sharathchintam <65326321+sharathchintam@users.noreply.github.com>
2021-08-18 19:06:32 -0400

    todo

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,21 +12,23 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
+            var total_2020_precipitation = measurements.Where(measure => measure.year == 2020).Select(measure => measure.precipitation).Sum();
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
             //
             // Heating Degree days have a mean temp of < 18C
             //   see: https://en.wikipedia.org/wiki/Heating_degree_day
-            //
 
-            // ?? TODO ??
-
-            //
             // Cooling degree days have a mean temp of >=18C
             //
 
-            // ?? TODO ??
+            var hotcold = measurements.GroupBy(y => y.year)
+                               .Select(s => new
+                               {
+                                   year = s.Key,
+                                   hdd = s.Where(h => h.meantemp < 18).Count(),
+                                   cdd = s.Where(c => c.meantemp >= 18).Count()
+                               });
 
             //
             // Most Variable days are the days with the biggest temperature
@@ -42,12 +44,24 @@ namespace WeatherLab
             //
             Console.WriteLine("Year\tHDD\tCDD");
 
-            // ?? TODO ??
+            foreach (var v in hotcold)
+            { Console.WriteLine($"{ v.year }\t{ v.hdd }\t{ v.cdd }"); }
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
-            // ?? TODO ??
+            var output = measurements.Select(s => new   { date = $"{s.year}-{s.month:d2}-{s.day:d2}",  delta = (s.maxtemp - s.mintemp) })
+                .OrderByDescending(d => d.delta);
+
+            int e = 0;
+            foreach (var i in output)
+            {
+                if (e < 5)
+                {   Console.WriteLine($"{i.date}\t{i.delta}");
+                    e++;    }
+                else
+                { break; }
+            }
         }
     }
 }
